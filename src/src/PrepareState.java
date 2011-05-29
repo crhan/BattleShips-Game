e1655efafcb70de49c08546aac3aa002003184fc
Crhan <crhan123@gmail.com>
2011-05-29 17:56:33 +0800

    解决prepare中的context问题

--- src/src/PrepareState.java
+++ src/src/PrepareState.java
@@ -5,17 +5,15 @@ public class PrepareState extends PlayState {
 	@Override
 	public boolean click(BattleShipTableModel _model, GridLocation location) {
 		BattleShipTableModel model;
-		if (!super.context.isMyTurn()) {
-			super.context.getAnotherPlayer();
-			
-			int _type = super.context.getCurrentShipType();
-			BattleShip _a = super.context.getShip(_type);
-			_a.setVertical(super.context.isShipVertical());
+		if (_model.isMyTurn()) {
+			int _type = _model.getCurrentShipType();
+			BattleShip _a = _model.getShip(_type);
+			_a.setVertical(_model.isShipVertical());
 			// if has ship, then try to set the location
 			try {
-				_a.setLocation(context.getSize(), location, super.context);
-				super.context.updateGrid();
-				super.context.setCurrentShipType(_type + 1);
+				_a.setLocation(context.getSize(), location, _model);
+				_model.updateGrid();
+				_model.setCurrentShipType(_type + 1);
 				return true;
 			} catch (GridOutOfBoundsException e) {
 				e.printStackTrace();
@@ -24,9 +22,9 @@ public class PrepareState extends PlayState {
 			} catch (BattleShipCrossOverException e) {
 				e.printStackTrace();
 			}
-			if (super.context.getCurrentShipType() > 5) {
-				super.context.setCurrentState(BattleShipTableModel.FIRE_STATE);
-				super.context.changePlayer();
+			if (_model.getCurrentShipType() > 5) {
+				_model.setCurrentState(BattleShipTableModel.FIRE_STATE);
+				_model.changePlayer();
 				return true;
 			}
 		}
